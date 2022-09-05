b24a9feb48e0b5d5800c2dbc405a9896341b3122
UhaiMong <uhaimarma2016@gmail.com>
2022-09-05 21:04:10 +0600

    upadate final

--- js/script.js
+++ js/script.js
@@ -2,13 +2,34 @@
 const api_key = `e77ede0c44cc2c609879f7296f5e9334`;
 const loadTemperature = async city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayTemperature(data);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayTemperature(data);
+    }
+    catch {
+        console.log(Error);
+    }
 }
 const displayTemperature = (data) => {
-    console.log(data);
-    const temp = document.getElementById('temperature');
-    temp.innerText = `${data.main.temp}`;
+    // const temp = document.getElementById('temperature');
+    // temp.innerText = `${data.main.temp}`;
+    setInerText('temperature', data.main.temp);
+    setInerText('cloud', data.weather[0].main);
+
+    console.log(data.weather[0].main);
 }
+
+const setInerText = (id, text) => {
+    const temp = document.getElementById(id);
+    temp.innerText = text;
+}
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    document.getElementById('city-name').innerText = city;
+
+    loadTemperature(city);
+});
 loadTemperature('Dhaka');
